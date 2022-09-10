9f204bb46f81a696ceba15411d8db9f6001c88f8
cdaringe <cdaringe@gmail.com>
2022-09-10 00:36:17 -0700

    feat: finish means

--- lib/means/means.ml
+++ lib/means/means.ml
@@ -5,21 +5,47 @@ end
 module Docket = struct
   open Int32
 
+  type 'v tree = Leaf | Node of 'v node [@@deriving show]
+
+  and 'v node = {
+    key : int32;
+    value : 'v;
+    left : 'v tree ref;
+    right : 'v tree ref;
+  }
+  [@@deriving show]
+
   type t
 
-  let rec insert key value = function
-    | [] -> [ (key, value) ]
-    | (k, v) :: xs as l ->
-        let open List in
-        if key < k then (key, value) :: l else (k, v) :: insert key value xs
+  let init key value = Node { key; value; left = ref Leaf; right = ref Leaf }
 
-  let in_range min max =
-    let fn acc (k, v) =
-      if k < min then (acc, `Continue)
-      else if k <= max then ((k, v) :: acc, `Continue)
-      else (acc, `Stop)
+  let rec insert k v n =
+    let child = if k < n.key then n.left else n.right in
+    match !child with Leaf -> child := init k v | Node n' -> insert k v n'
+
+  let find_root_in_range min max n =
+    let in_range n = n.key >= min && n.key <= max in
+    let rec find n =
+      match n with
+      | Leaf -> None
+      | Node n ->
+          if in_range n then Some n
+          else
+            (* let _ = Eio.traceln "-> %i" (Int32.to_int n.key) in *)
+            let child = if n.key < min then n.right else n.left in
+            find !child
     in
-    Clist.fold_while fn []
+    find n
+
+  let rec visit_in_range (on_visit : 'a node -> unit) min max n =
+    let { key; left; right; _ } = n in
+    let is_out_of_range = key < min || key > max in
+    if is_out_of_range then () else on_visit n;
+    (match !left with Node n' -> visit_in_range on_visit min max n' | _ -> ());
+    (match !right with
+    | Node n' -> visit_in_range on_visit min max n'
+    | _ -> ());
+    ()
 
   let sum = List.(fold_left add zero)
 
@@ -41,7 +67,6 @@ module Req = struct
 
   let bytes_to_int cl =
     cl |> List.map Char.chr |> List.to_seq |> Bytes.of_seq |> fun b ->
-    (* Eio.traceln "bytes_to_string: %s" (Bytes.to_string b); *)
     Bytes.get_int32_be b 0
 
   type int_list = int list [@@deriving show]
@@ -50,7 +75,6 @@ module Req = struct
     if List.length l < 4 then
       let c = Eio.Buf_read.any_char b in
       let code = Char.code c in
-      (* Eio.traceln "char: %c, code: %i" c code; *)
       parse_int_bytes (code :: l) b
     else l |> List.rev
 
@@ -61,20 +85,13 @@ module Req = struct
   let i_of_reader b =
     let time = parse_int_bytes [] b in
     let price = parse_int_bytes [] b in
-    (* Eio.traceln "%s, %s" (show_int_list time) (show_int_list price); *)
     Insert { time = bytes_to_int time; price = bytes_to_int price }
 
   let of_reader buf_r =
-    (* let trace = Eio.traceln in *)
     let mode = Eio.Buf_read.any_char buf_r in
-    (* trace "%c: query type" mode; *)
-    let tap_show r =
-      (* trace "%s" (show r); *)
-      r
-    in
     match mode with
-    | 'Q' -> q_of_reader buf_r |> tap_show |> Result.ok
-    | 'I' -> i_of_reader buf_r |> tap_show |> Result.ok
+    | 'Q' -> q_of_reader buf_r |> Result.ok
+    | 'I' -> i_of_reader buf_r |> Result.ok
     | c ->
         Error (Errors.InvalidPayload (Printf.sprintf "'%c' invalid msg type" c))
 end
@@ -84,26 +101,44 @@ module Res = struct
   [@@deriving show, yojson { strict = false }]
 end
 
-let count = ref 0
-
 let handle_stream flow _ =
-  let data = ref [] in
-  let reader = Eio.Buf_read.of_flow flow ~max_size:9 in
+  let sf s = Printf.sprintf s in
+  let log s = Eio.traceln "%s" s in
+  let data = ref Docket.Leaf in
+  let reader = Eio.Buf_read.of_flow flow ~max_size:1_000_000 in
   try
     while true do
-      count := !count + 1;
-      let _ =
-        if !count mod 1000 = 0 then Eio.traceln "count: %i" !count else ()
-      in
       match Req.of_reader reader with
-      | Ok (Req.Insert { time; price }) ->
-          data := Docket.insert time price !data
-      | Ok (Req.Query { min; max }) ->
-          Docket.(in_range min max !data |> values |> mean) |> fun mean ->
-          let b = Bytes.create 4 in
-          Bytes.set_int32_be b 0 mean;
-          Server.send_bytes ~flow b;
-          ()
+      | Ok (Req.Insert { time; price }) -> (
+          match !data with
+          | Docket.Leaf -> data := Docket.init time price
+          | Docket.Node node -> Docket.insert time price node |> ignore)
+      | Ok (Req.Query { min; max }) -> (
+          match !data with
+          | Docket.Leaf -> raise (Failure "no data")
+          | Docket.Node _ as t ->
+              let total = ref Int64.zero in
+              let count = ref 0 in
+              let on_visit Docket.{ value; _ } =
+                (total := Int64.(add !total (of_int32 value)));
+                incr count;
+                ()
+              in
+              Docket.(
+                find_root_in_range min max t |> function
+                | Some n -> visit_in_range on_visit min max n
+                | None -> ());
+              let mean =
+                let open Int64 in
+                if !count = 0 then zero else div !total (of_int !count)
+              in
+              let b = Bytes.create 4 in
+              log
+              @@ sf "min,max,mean,count\n%i,%i,%s,%i" (Int32.to_int min)
+                   (Int32.to_int max) (Int64.to_string mean) !count;
+              Bytes.set_int32_be b 0 (Int64.to_int32 mean);
+              Server.send_bytes ~flow b;
+              ())
       | Error (Errors.InvalidPayload msg) -> Eio.traceln "Parse failed: %s" msg
     done
   with End_of_file -> ()
