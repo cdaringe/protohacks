267231c52640ab8bbb8790e9f1595b30450f9768
cdaringe <cdaringe@gmail.com>
2022-09-26 14:43:22 -0700

    PLEASE OCAML WORK WITH ME BRO

--- lib/budgetchat/budgetchat.ml
+++ lib/budgetchat/budgetchat.ml
@@ -1,93 +1,183 @@
+(*
+# Solution
+- Each socket gets a handler, H
+- H may read freely from its socket
+- H may never write to its socket
+- H may request writes to its socket AND peer sockets
+- Each socket gets a socket writer, W
+- Each W maintains a queue of writes, Wi_q
+- Each H may add to _any_ Wi_q
+- Each W depletes its queue
+- H may purge its associated W_q
+*)
+
 open Effect
 open Effect.Deep
 module MapKStr = Map.Make (String)
 
-module Pool = struct
-  type t
-  type _ Effect.t += Connect : string -> unit Effect.t
-  type _ Effect.t += Announce : string -> unit Effect.t
-  type _ Effect.t += Broadcast : string -> unit Effect.t
-  type _ Effect.t += Disconnect : string -> unit Effect.t
+let log = Eio.traceln
+let create_msg name body = `Msg (name, body)
 
-  let announce m = perform (Announce m)
-  let broadcast prefix = perform (Broadcast prefix)
-  let join name = perform (Connect name)
-  let leave name = perform (Disconnect name)
-end
+let create_peer_msgs ~peer_names body =
+  List.map (fun member_name -> create_msg member_name body) peer_names
 
 module Handshake = struct
-  type _ Effect.t += Greet : string -> unit Effect.t
+  type _ Effect.t += Greet : unit Effect.t
   type _ Effect.t += Identify : string Effect.t
 
-  let init _ = perform (Greet "guten tag")
+  let verify_identity id =
+    if Cchar.is_alphanum_str id && String.length id > 0 then Ok (String.trim id)
+    else Error ("invalid name: '" ^ id ^ "'")
 
-  let verify_identity () =
-    let id = perform Identify in
-    if Cchar.is_alphanum_str id && String.length id > 0 then id
-    else raise (Failure ("invalid: " ^ id))
+  module Handlers = struct
+    open Server
+
+    let run ~client (type a) (e : a Effect.t) =
+      let flow, _ = client in
+      match e with
+      | Greet ->
+          Some
+            (fun (k : (a, _) continuation) ->
+              send_line ~flow "Welcome to budgetchat! What shall I call you?";
+              continue k ())
+      | Identify -> Some (fun k -> continue k (Server.read_line_term_char flow))
+      | _ -> None
+  end
 end
 
-let handle_stream _ =
-  let name = Handshake.(init () |> verify_identity) in
-  let open Pool in
-  join name;
-  announce name;
-  broadcast ("[" ^ name ^ "] ");
-  leave name;
-  ()
+module Pool = struct
+  type t
+  type _ Effect.t += Announce : string -> unit Effect.t
+  type _ Effect.t += Broadcast : string -> unit Effect.t
+  type _ Effect.t += Join : string -> unit Effect.t
+  type _ Effect.t += Disconnect : string -> unit Effect.t
 
-let handle_effects ~pool ~flow f =
-  let open Server in
-  let name = ref "" in
-  let send_to_others msg =
-    let send_ignore_me name' flow =
-      if !name != name' then send_line ~flow msg
-    in
-    MapKStr.iter send_ignore_me !pool
-  in
+  let keys ?(filter = fun _ -> true) t =
+    MapKStr.fold
+      (fun k _v acc -> match filter k with true -> k :: acc | _ -> acc)
+      t []
 
-  try_with f ()
+  let keys_without k = keys ~filter:(fun k' -> k != k')
+
+  module Handlers = struct
+    open Server
+
+    let prefix name = "[" ^ name ^ "] "
+
+    let run ~send ~pool ~flow (type a) (e : a Effect.t) =
+      match e with
+      | Announce name ->
+          Some
+            (fun (k : (a, _) continuation) ->
+              let peer_names = keys_without name !pool in
+              let body = String.concat ", " peer_names in
+              let current_peers_msg = "* The room contains: " ^ body in
+              log "\tcurrent_peers_msg: %s" current_peers_msg;
+              send (create_msg name current_peers_msg);
+              let enter_msg = "* " ^ name ^ " has entered the room" in
+              let peer_msgs = create_peer_msgs ~peer_names enter_msg in
+              log "\tenter_msg: %s" enter_msg;
+              List.iter send peer_msgs;
+              continue k ())
+      | Broadcast name ->
+          Some
+            (fun k ->
+              let on_line msg =
+                let peer_names = keys_without name !pool in
+                List.iter send
+                  (create_peer_msgs ~peer_names (prefix name ^ msg))
+              in
+              log "waiting for great stuff";
+              (try read_lines ~on_line ~flow
+               with exn ->
+                 log "[%s: halting broadcast] (%s)" name
+                   (Printexc.to_string exn));
+              continue k ())
+      | Disconnect name ->
+          Some
+            (fun k ->
+              pool := MapKStr.remove name !pool;
+              let peer_names = keys_without name !pool in
+              List.iter send
+                (create_peer_msgs ~peer_names
+                   (prefix name ^ " has left the room"));
+              continue k ())
+      | Join name ->
+          Some
+            (fun k ->
+              pool := MapKStr.add name flow !pool;
+              continue k ())
+      | _ -> None
+  end
+end
+
+let handle_effects ~enqueue ~pool ~flow ~client f =
+  match_with f ()
     {
+      retc =
+        (fun v ->
+          log "xxx finished connection";
+          v);
+      exnc = (fun e -> log "steam exiting: %a" Fmt.exn e);
       effc =
         (fun (type a) (e : a Effect.t) ->
-          match e with
-          | Handshake.Greet msg ->
-              Some
-                (fun (k : (a, _) continuation) ->
-                  send_line ~flow msg;
-                  continue k ())
-          | Handshake.Identify ->
-              Some
-                (fun k ->
-                  name := read_line ();
-                  continue k !name)
-          | Pool.Announce name ->
-              Some
-                (fun k ->
-                  let other_names =
-                    MapKStr.fold
-                      (fun k _ xs -> if k = name then xs else k :: xs)
-                      !pool []
-                  in
-                  let others = String.concat ", " other_names in
-                  send_line ~flow ("* The room contains: " ^ others);
-                  send_to_others ("* " ^ name ^ " has entered the room");
-                  continue k ())
-          | Pool.Broadcast prefix ->
-              Some
-                (fun k ->
-                  let on_line msg = send_to_others (prefix ^ msg) in
-                  read_lines ~on_line ~flow;
-                  continue k ())
-          | Pool.Connect name ->
-              Some
-                (fun k ->
-                  pool := MapKStr.add name flow !pool;
-                  continue k ())
-          | _ -> None);
+          List.find_map
+            (fun run -> run e)
+            [
+              Pool.Handlers.run ~send:enqueue ~pool ~flow;
+              Handshake.Handlers.run ~client;
+            ]);
     }
 
+let chat _ =
+  log "pregreet";
+  perform Handshake.Greet;
+  let name = perform Handshake.Identify in
+  match Handshake.verify_identity name with
+  | Error msg -> log "invalid identity %s" msg
+  | Ok name ->
+      let open Pool in
+      let participate _ =
+        perform (Join name);
+        perform (Announce name);
+        perform (Broadcast name)
+      in
+      let finally _ =
+        try
+          log "trying to leave pool";
+          perform (Disconnect name)
+        with _ -> ()
+      in
+      Fun.protect ~finally participate
+
 let listen ~env ~port =
-  let pool = ref MapKStr.empty in
-  let fn flow _ = handle_effects ~pool ~flow handle_stream in
-  Server.listen ~env ~port ~fn
+  let open Eio in
+  Switch.run (fun sw ->
+      let pool = ref MapKStr.empty in
+      let q = Queue.create () in
+      let depleting = ref false in
+      let rec deplete_q _ =
+        depleting := true;
+        match Queue.take_opt q with
+        | Some (`Msg (name, msg)) ->
+            (match MapKStr.find_opt name !pool with
+            | Some flow -> (
+                log "[q] (to %s) %s" name msg;
+                try Server.send_line ~flow msg with _ -> log "bummer")
+            | _ -> ())
+            |> deplete_q
+        | _ ->
+            depleting := false;
+            log "[q] empty";
+            ()
+      in
+      let enqueue k =
+        Queue.push k q;
+        match !depleting with true -> () | false -> deplete_q ()
+      in
+      let swo = Some sw in
+      Server.listen ~swo ~env ~port
+        ~fn:(fun flow addr ->
+          log "Wat";
+          handle_effects ~enqueue ~pool ~flow ~client:(flow, addr) chat)
+        ())
--- lib/server/server.ml
+++ lib/server/server.ml
@@ -3,8 +3,7 @@ open Eio.Net
 module Read = Eio.Buf_read
 module Write = Eio.Buf_write
 
-let listen ~env ~port ~fn =
-  Switch.run @@ fun sw ->
+let listen_ ~sw ~env ~port ~fn =
   let net = Eio.Stdenv.net env in
   let addr = `Tcp (Ipaddr.V4.any, port) in
   let socket = listen ~sw ~backlog:1000 net addr in
@@ -17,6 +16,11 @@ let listen ~env ~port ~fn =
     accept_fork socket ~sw ~on_error fn
   done
 
+let listen ?(swo = None) ~env ~port ~fn () =
+  match swo with
+  | None -> Switch.run @@ fun sw -> listen_ ~sw ~env ~port ~fn
+  | Some sw -> listen_ ~sw ~env ~port ~fn
+
 let read_lines_exn ~buf ~on_line =
   while Read.eof_seen buf = false do
     on_line (Read.line buf)
@@ -26,6 +30,10 @@ let read_lines_exn ~buf ~on_line =
 let read_bytes ~flow num_bytes =
   let r = Read.of_flow flow ~initial_size:num_bytes ~max_size:num_bytes in
   r
+
+let read_line_term_char ?(term = '\n') flow =
+  let buf = Read.of_flow flow ~initial_size:100 ~max_size:1_000_000 in
+  Read.take_while (fun c -> c != term) buf
 
 let read_lines ~flow ~on_line =
   let buf = Read.of_flow flow ~initial_size:100 ~max_size:1_000_000 in
@@ -44,10 +52,11 @@ let send_line ~flow text =
   let write_flush t =
     (* probably a frivolous flush, but protohackers seems to be less
        finicky with it? could be blind luck. *)
-    Write.flush t;
-    Write.string t line;
-    Write.flush t
+    Write.string t line
   in
-  Write.with_flow flow write_flush
+  try
+    Write.with_flow flow write_flush;
+    Eio.traceln "(sent line: %s)" (String.trim line)
+  with _ -> Eio.traceln "dude it failed"
 
 let send_ndjson ~tojson ~flow t = tojson t |> send_line ~flow
